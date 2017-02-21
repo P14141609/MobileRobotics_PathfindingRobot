a1d0a1c26f4d1c3caca76ef5586cc99eccb7bb58
Ross-Gosling <p14141609@my365.dmu.ac.uk>
2017-02-21 23:25:04 +0000

    Renamed RenderWindow Title

--- Projects/RobotProject/src/main.cpp
+++ Projects/RobotProject/src/main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char **argv)
 {
 	// SFML Setup
 	// Instantiates window
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "Pathfinding Representation - Coursework Submission 2 (Part2) - Mobile Robotics - P14141609", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Pathfinding Representation - Coursework-2 (Submission 2) - Mobile Robotics - P14141609", sf::Style::Default);
 
 	// Aria Setup
 	Aria::init();
